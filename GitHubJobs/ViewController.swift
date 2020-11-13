760497f030b0b6c8c84286e183bc04843ba8d6e1
darik88 <mr.darik88@gmail.com>
2020-11-14 02:17:38 +0300

    Initial Commit

---
+++ GitHubJobs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubJobs
+//
+//  Created by Айдар Рахматуллин on 14.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
